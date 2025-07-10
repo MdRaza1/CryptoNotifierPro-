484635c82c61dc3abd435cac586e4721a3c543b1
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-10 22:53:25 +0530

    fix: support_bot

--- support_bot.py
+++ support_bot.py
@@ -1,1 +1,6 @@
-# Support bot auto-reply placeholder
+# support_bot.py
+
+from pyrogram.types import Message
+
+async def support_handler(client, message: Message):
+    await message.reply("👨‍💻 Our 24x7 support will contact you soon. For now, send your query to @CryptoRaza0")
