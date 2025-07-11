e4690f0d59531442e410fcad4fa0439e5607ad0a
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 09:35:21 +0530

    Delete payment_gateway.py

--- payment_gateway.py
+++
@@ -1,6 +0,0 @@
-# payment_gateway.py
-
-from pyrogram.types import Message
-
-async def handle_payment(client, message: Message):
-    await message.reply("💳 Payment system is under development. Please wait for updates.")
