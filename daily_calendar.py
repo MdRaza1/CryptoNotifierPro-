049c261df954ac1ff3c7583b83e8e770ee2a3ea6
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 09:35:03 +0530

    Delete daily_calendar.py

--- daily_calendar.py
+++
@@ -1,6 +0,0 @@
-# daily_calendar.py
-
-from pyrogram.types import Message
-
-async def get_calendar(client, message: Message):
-    await message.reply("📅 Your daily trade calendar is under construction. Coming soon!")
