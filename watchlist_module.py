8786b748188c4ac2101d38fa9d3cf04cf94600c1
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 09:36:42 +0530

    Delete watchlist_module.py

--- watchlist_module.py
+++
@@ -1,6 +0,0 @@
-# watchlist_module.py
-
-from pyrogram.types import Message
-
-async def manage_watchlist(client, message: Message):
-    await message.reply("📌 This is your personal watchlist feature. Coming soon with alerts!")
