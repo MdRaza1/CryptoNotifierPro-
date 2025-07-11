83766f116cea1ad248c03610e802d54ecd6d7c19
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 10:49:32 +0530

    Delete strategy_pdf.py

--- strategy_pdf.py
+++
@@ -1,4 +0,0 @@
-from pyrogram.types import Message
-
-async def send_strategy_pdf(client, message: Message):
-    await message.reply_document("https://example.com/sample-strategy.pdf", caption="📘 Here is your strategy guide.")
