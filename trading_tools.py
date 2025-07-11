7a40dee5fe8f6b48d24a5fefece5f70659f07141
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 10:49:57 +0530

    Delete trading_tools.py

--- trading_tools.py
+++
@@ -1,4 +0,0 @@
-from pyrogram.types import Message
-
-async def trading_tools_handler(client, message: Message):
-    await message.reply("🛠️ Use /lotcalc, /risk, /badge etc. Tools will be available soon.")
