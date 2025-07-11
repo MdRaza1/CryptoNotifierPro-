03df9e32a48b36240fe4ef950416705bccab8c0d
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 09:36:13 +0530

    Delete support_bot.py

--- support_bot.py
+++
@@ -1,6 +0,0 @@
-# support_bot.py
-
-from pyrogram.types import Message
-
-async def support_handler(client, message: Message):
-    await message.reply("👨‍💻 Our 24x7 support will contact you soon. For now, send your query to @CryptoRaza0")
