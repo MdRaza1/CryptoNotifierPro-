83c34408aa4afa774fd448eef49430721e5bf861
MdRaza1 <87989262+MdRaza1@users.noreply.github.com>
2025-07-11 10:48:04 +0530

    Delete referral_system.py

--- referral_system.py
+++
@@ -1,4 +0,0 @@
-from pyrogram.types import Message
-
-async def handle_referral(client, message: Message):
-    await message.reply("🎁 Your referral link: https://t.me/CryptoNotifierPro_bot?start=ref123")
